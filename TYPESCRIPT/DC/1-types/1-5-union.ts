ee5823e0cac0e20c3a72e6c0ee4758f6515695ab
shp7408 <62336151+shp7408@users.noreply.github.com>
2022-06-05 23:15:57 +0900

    union types 공부

---
+++ TYPESCRIPT/DC/1-types/1-5-union.ts
@@ -0,0 +1,61 @@
+{
+    /*
+     * Union Types: OR
+    */
+    type Direction = 'left' | 'right' | 'up' | 'down';
+    function move(direction: Direction) {
+        console.log(direction);
+    }
+
+    move('down');
+    //    move('lala'); // error
+    type TileSize = 8 | 16 | 32;
+    const tile1: TileSize = 16;
+    //    const tile2: TileSize = 33; // error
+
+    // function: login -> success, fail
+    //    type LoginResult = 'success' | 'fail';
+    //    function showLoginResult(result:LoginResult) {
+    //         if (result === 'success') {
+    //             console.log(result);
+    //         } else {
+    //             console.log(result, "why?");
+    //         }
+    //     }
+
+    type SuccessState = {
+        response: {
+            body: string;
+        };
+    };
+
+    type FailState = {
+        reason: string;
+    };
+
+    type LoginState = SuccessState | FailState;
+    function login(id: string, password: string): LoginState {
+        return {
+            response: {
+                body: 'logged in!',
+            },
+        };
+    };
+
+    // printLoginState(state: LoginState)
+    // success -> body
+    // fail -> reason 출력
+
+    function printLoginState(state: LoginState) {
+        if ('response' in state) {
+            console.log(state.response.body);
+        } else if ('reason' in state) {
+            console.log(state.reason);
+        }
+    };
+
+    const state1:FailState = {
+        reason: "nullllll",
+    }
+    printLoginState(state1);
+}
